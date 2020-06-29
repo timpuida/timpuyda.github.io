0a67fe3c8998548ed4f4bc6a55bbc65ca4ab5c10
timpuyda <31927275+timpuyda@users.noreply.github.com>
2020-06-29 17:35:12 +0300

    Add files via upload

---
+++ TEST/JS/popups.js
@@ -0,0 +1,31 @@
+﻿const wrap = document.querySelector(".wrap-items");
+const itemMenus = document.querySelectorAll(".item-menu");
+const popupOpen = document.querySelectorAll(".popup-opening");
+const overlay = document.querySelector("#overlay");
+const block = document.querySelectorAll('.item');
+const addBtn = document.querySelector('.add-box')
+const popupAdd = document.querySelector('.popup-new')
+
+addBtn.addEventListener('click', function(event){
+	popupAdd.classList.toggle('hide');
+	overlay.classList.toggle('hide');
+})
+wrap.addEventListener('click', function(event){
+	for(let i = 0; i<itemMenus.length;i++){	
+			if(event.target ==itemMenus[i]) {
+				if(event.target==itemMenus[i]){
+						for(let i of block) i.style.zIndex = '';
+						popupOpen[i].classList.toggle('hide');
+						overlay.classList.toggle('hide');
+						block[i].style.zIndex = '2';
+				}
+			}
+		}
+})
+overlay.addEventListener('click', function(event){
+	for(let pops of popupOpen){
+		pops.classList.add('hide');
+	}
+	overlay.classList.add('hide')
+	popupAdd.classList.add('hide');
+})
