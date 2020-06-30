baa07c01a5cc52dfb5afde5b98ce4b41b2275617
timpuyda <31927275+timpuyda@users.noreply.github.com>
2020-06-30 13:23:45 +0300

    Update popups.js

--- TEST/JS/popups.js
+++ TEST/JS/popups.js
@@ -3,33 +3,32 @@ const itemMenus = document.querySelectorAll(".item-menu");
 const popupOpen = document.querySelectorAll(".popup-opening");
 const overlay = document.querySelector("#overlay");
 const block = document.querySelectorAll('.item');
-const addBtn = document.querySelector('.add-box')
-const popupAdd = document.querySelector('.popup-new')
-
+const addBtn = document.querySelector('.add-box');
+const popupAdd = document.querySelector('.popup-new');
+const closeBtn = document.querySelectorAll('.item-menu-close');
+const menuCircles = document.querySelectorAll('.item-menu-circles');
 addBtn.addEventListener('click', function(event){
+	event.preventDefault();
 	popupAdd.classList.toggle('hide');
 	overlay.classList.toggle('hide');
 })
 wrap.addEventListener('click', function(event){
 	for(let i = 0; i<itemMenus.length;i++){	
-			if(event.target ==itemMenus[i]) {
-				if(event.target==itemMenus[i]){
-						for(let i of block) i.style.zIndex = '';
-						popupOpen[i].classList.toggle('hide');
-						overlay.classList.toggle('hide');
-						block[i].style.zIndex = '2';
-						block[i].classList.toggle('absolute')
-				}
+			if(event.target==itemMenus[i]){
+				event.preventDefault();
+					for(let i of block) i.style.zIndex = '';
+					popupOpen[i].classList.toggle('hide');
+					overlay.classList.toggle('hide');
+					block[i].style.zIndex = '2';
+					closeBtn[i].classList.toggle('hide');
+					menuCircles[i].classList.toggle('hide');
 			}
 		}
 })
 overlay.addEventListener('click', function(event){
-	for(let pops of popupOpen){
-		pops.classList.add('hide');
-	}
-	for(let item of block){
-		item.classList.remove('absolute')
-	}
-	overlay.classList.add('hide')
+	closeBtn.forEach((i)=> i.classList.add('hide'));
+	menuCircles.forEach(i=>i.classList.remove('hide'));
+	popupOpen.forEach(i=>i.classList.add('hide'))
+	overlay.classList.add('hide');
 	popupAdd.classList.add('hide');
 })
